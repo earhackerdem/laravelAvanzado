5bc3dc4a5271875cb193fa458b7cb51ed620117a
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-07-07 10:58:43 -0500

    3.1 - Categorias

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,16 +5,15 @@ namespace Database\Seeders;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CategorySeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-       $this->call(ProductSeeder::class);
-       $this->call(CategorySeeder::class);
+        Category::factory(5)->create();
     }
 }
